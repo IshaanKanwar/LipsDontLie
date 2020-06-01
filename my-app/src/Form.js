37de84bca8b963454ca1424c9796244e731f9c97
IshaanKanwar <ishaan1168@gmail.com>
2020-06-01 23:55:54 +0530

    IK - Last commit

--- my-app/src/Form.js
+++ my-app/src/Form.js
@@ -3,6 +3,8 @@ import Button from '@material-ui/core/Button';
 import axios from 'axios';
 import './App.css';
 import { Typography } from '@material-ui/core';
+import { withRouter } from 'react-router-dom';
+// import { useHistory } from 'react-router-dom';
 
 export default class Form extends Component{
 	constructor(props) {
@@ -10,22 +12,29 @@ export default class Form extends Component{
 		  	this.state = {
 			selectedFile: null
 		}
+		this.routeChange = this.routeChange.bind(this);
 	}
+
+	routeChange() {
+		let path = `/graph`;
+		this.props.history.push(path);
+		this.onClickHandler();
+	  }
 	
-	onChangeHandler=event=>{
-		this.setState({
-			selectedFile: event.target.files[0],
-			loaded: 0,
-		  })
-		console.log(event.target.files[0])
+	// onChangeHandler=event=>{
+	// 	this.setState({
+	// 		selectedFile: event.target.files[0],
+	// 		loaded: 0,
+	// 	  })
+	// 	console.log(event.target.files[0])
 	
-	}
+	// }
 	
 	onClickHandler = () => {
 		const data = new FormData() 
 		data.append('file', this.state.selectedFile)
-		console.log(this.state.selectedFile);
-		axios.post(`https://storageupload.s3.us-east-2.amazonaws.com/${this.state.selectedFile.name}`, data, { // receive two parameter endpoint url ,form data... Use firebase or aws s3 storage http://localhost:8000/upload
+		// console.log(this.state.selectedFile);
+		axios.post(`http://wordpredictionservice-env.eba-weqaidmq.us-east-2.elasticbeanstalk.com/predict`, data, { // receive two parameter endpoint url ,form data... Use firebase or aws s3 storage http://localhost:8000/upload
       })
       .then(res => { // then print response status
 		console.log(res.statusText)
@@ -39,7 +48,7 @@ export default class Form extends Component{
             <div>
 			 <Typography variant="h2" align="center" color="textPrimary">Upload Your File </Typography>
 			 <input type="file" name="file" onChange={this.onChangeHandler}/>
-			 <Button type="outlined" color="primary" disableElevation onClick={this.onClickHandler}>Upload</Button>
+			 <Button type="outlined" color="primary" disableElevation onClick={this.routeChange}>Upload</Button>
             </div>
         )
     }
--- my-app/src/Graph.js
+++ my-app/src/Graph.js
@@ -6,17 +6,15 @@ export default class Graph extends Component{
         super(props);
         this.state={
           chartData:{
-            labels:['A','B','C','D','E','F','G'],
+            labels:['hello','hollow','how','shallow','slow'],
             datasets:[{
               label:'Probability',
               data:[
+                0.65,
+                0.2,
                 0.11,
-                0.23,
-                0.42,
-                0.04,
-                0.09,
-                0.01,
-                0.1
+                0.03,
+                0.01
               ],
               backgroundColor:[
                 'rgba(255,99,132,0.6)',
@@ -24,9 +22,7 @@ export default class Graph extends Component{
                 'rgba(255,206,86,0.6)',
                 'rgba(75,192,192,0.6)',
                 'rgba(153,102,255,0.6)',
-                'rgba(255,159,64,0.6)',
-                'rgba(255,99,132,0.6)',
-                'rgba(252,142,255,0.6)'
+                'rgba(255,159,64,0.6)'
               ]
             }]
           }
